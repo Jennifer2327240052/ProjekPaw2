2f5e967673c7952e56609cb62aa62d78c6a73156
Jennifer2327240052 <jennifer_2327240052@mhs.mdp.ac.id>
2025-12-19 22:29:23 +0700

    push

--- angular-login-dashboard/src/app/app.routes.ts
+++ angular-login-dashboard/src/app/app.routes.ts
@@ -1,25 +1,29 @@
-
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './Auth/Login.component';
 import { DashboardComponent } from './Pages/dashboard.component';
-
-
+import { DashboardHomeComponent } from './Pages/dashboard-home.component';
 
 import { BillListComponent } from './Pages/bill-list.component';
 import { AddBillComponent } from './Pages/add-bill.component';
 import { EditBillComponent } from './Pages/edit-bill.component';
+
 import { ScheduleListComponent } from './Pages/schedule-list.component';
 import { AddScheduleComponent } from './Pages/add-schedule.component';
-import { DashboardHomeComponent } from './Pages/dashboard-home.component';
+
 import { StudentListComponent } from './Pages/student-list.component';
 import { AddStudentComponent } from './Pages/add-student.component';
 import { EditStudentComponent } from './Pages/edit-student.component';
+
 import { TeacherListComponent } from './Pages/teacher-list.component';
 import { AddTeacherComponent } from './Pages/add-teacher.component';
 import { EditTeacherComponent } from './Pages/edit-teacher.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -29,14 +33,13 @@ export const routes: Routes = [
       { path: 'add-bill', component: AddBillComponent },
       { path: 'edit-bill/:id', component: EditBillComponent },
       { path: 'schedule-list', component: ScheduleListComponent },
+      { path: 'add-schedule', component: AddScheduleComponent },
       { path: 'student-list', component: StudentListComponent },
       { path: 'add-student', component: AddStudentComponent },
       { path: 'edit-student/:id', component: EditStudentComponent },
       { path: 'teacher-list', component: TeacherListComponent },
       { path: 'add-teacher', component: AddTeacherComponent },
       { path: 'edit-teacher/:id', component: EditTeacherComponent },
-      { path: 'add-schedule', component: AddScheduleComponent },
-      // tambahkan child routes lain jika perlu
     ]
   }
 ];
